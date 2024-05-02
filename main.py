439e31a9c3e5edb6157a6f147317737cb25fed36
Franklin Mvoi <mvoifranklin@gmail.com>
2024-05-02 22:38:03 +0300

    completed

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ args = vars(ap.parse_args())
 # to the new height, clone it, and resize it
 image = cv2.imread(args["image"])
 ratio = image.shape[0] / 500.0
-orgin = image.copy()
+orig = image.copy()
 image = imutils.resize(image, height = 500)
 
 # convert the image to grayscale , blur and find edges
@@ -25,11 +25,11 @@ gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
 # show the original image and the edge detected image
-# print("STEP 1: Edge detection")
-# cv2.imshow("Image", image)
-# cv2.imshow("Edged", edged)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+print("STEP 1: Edge detection")
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 ## 2. FINDING CONTOURS
 # find the contours in the edged image, keeping only
@@ -54,5 +54,22 @@ for c in cnts:
 print("STEP 2: Find contours of the paper")
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outliine", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# apply the four point transform to obtain a top-down
+# view of the original image
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+# convert the warped image to grayscale, the  threshold it
+# to give that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+# show the original and scanned images
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
