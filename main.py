78191ab4d32bc36c4d897409f6df036ce3642f3e
Franklin Mvoi <mvoifranklin@gmail.com>
2024-05-02 22:29:46 +0300

    finding contours adn edge detection of the document image

--- main.py
+++ main.py
@@ -1,3 +1,58 @@
 import numpy as np
 import cv2
 import imutils
+from skimage.filters import threshold_local
+import argparse
+from transform import four_point_transform
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+                help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+## 1. EDGE DETECTION
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orgin = image.copy()
+image = imutils.resize(image, height = 500)
+
+# convert the image to grayscale , blur and find edges
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+# print("STEP 1: Edge detection")
+# cv2.imshow("Image", image)
+# cv2.imshow("Edged", edged)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+## 2. FINDING CONTOURS
+# find the contours in the edged image, keeping only
+# the largest oens, and inits the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+
+# loop over the contours
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    
+    # if our approximated contour has 4 points, then ww
+    # can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of the paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outliine", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
